3e07fc850980f7ed49d7b9db4943b04a44a9a12a
PalDew <palashofbackup@gmail.com>
2025-04-22 00:22:46 +0530

    new emacs config no evil

--- emacs/custom-vars.el
+++ emacs/custom-vars.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+	 '(cape consult corfu diff-hl diminish doom-modeline eat gruvbox-theme
+					lsp-pyright magit marginalia nov orderless org-superstar
+					pdf-tools projectile rainbow-delimiters toc-org vertico
+					yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ emacs/elpa/corfu-20250419.530/corfu-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "corfu" "20250419.530"
+  "COmpletion in Region FUnction."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/corfu"
+  :commit "50a3fbf1d4e6a39c618a86d931bee9584306a8dd"
+  :revdesc "50a3fbf1d4e6"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
